7d688e5a2139cf990da739e608181f77f09a4c76
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-09-11 16:30:44 +0300

    Adding a task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import Task from "./components/Task";
 
-function App({ tasks }) {
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(task, parentId) {
+    setTasks((tasks) => addSubtask(tasks, task, parentId));
+  }
+
   return (
     <ul>
       {tasks.map((task) => (
-        <Task key={task.id} {...task} />
+        <Task key={task.id} {...task} addTask={addTask} />
       ))}
     </ul>
   );
+}
+
+function addSubtask(tasks, subtask, parentId) {
+  return tasks.map((task) => {
+    if (task.id === parentId) {
+      return { ...task, subtasks: [...task.subtasks, subtask] };
+    } else {
+      return {
+        ...task,
+        subtasks: addSubtask(task.subtasks, subtask, parentId),
+      };
+    }
+  });
 }
 
 export default App;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,23 @@
-function Task({ id, name, checked, subtasks }) {
+function Task({ id, name, checked, subtasks, addTask }) {
   return (
     <li>
       {name}
+      <button type="button" onClick={() => addTask(createTask("Task"), id)}>
+        Add
+      </button>
       {subtasks.length > 0 && (
         <ul>
           {subtasks.map((subtask) => (
-            <Task key={subtask.id} {...subtask} />
+            <Task key={subtask.id} {...subtask} addTask={addTask} />
           ))}
         </ul>
       )}
     </li>
   );
+}
+
+function createTask(name) {
+  return { id: "", name, checked: false, subtasks: [] };
 }
 
 export default Task;
